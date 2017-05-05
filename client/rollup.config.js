6dff6726b0c4d5a2e7706038446dccfc92ca3d26
Lance Lafontaine <lance.lafontaine92@gmail.com>
2017-05-05 12:43:54 -0400

    Adds a basic front-end workflow using Gulp to build and serve our HTML/CSS/JS files.

---
+++ client/rollup.config.js
@@ -0,0 +1,11 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'src/js/main.js',
+  plugins: [
+    babel(),
+    uglify()
+  ],
+  format: 'umd'
+};
---
+++ client/src/js/main.js
@@ -0,0 +1,3 @@
+(() => {
+  alert('haha it works again')
+})();
