2cb81a5aefbb6c95601dd4ec78ff6f2144668e58
Lance Lafontaine <lance.lafontaine92@gmail.com>
2017-07-16 18:47:51 -0400

    Fix linting issues

--- client/test/unit/specs/global.spec.js
+++ client/test/unit/specs/global.spec.js
@@ -3,6 +3,6 @@ import global from '@/global'
 describe('global.js', () => {
   it('should be able to call defaultUserError', () => {
     expect(global.defaultUserError).to.be.a('function')
-    expect(global.defaultUserError({'something':'went wrong'})).to.be.undefined
+    expect(global.defaultUserError({'something': 'went wrong'})).to.be.undefined
   })
 })
